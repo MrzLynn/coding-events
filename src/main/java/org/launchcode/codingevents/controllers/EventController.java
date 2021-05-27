631860cc51823f5946d3529058a9bc1d5ac90d78
Your Name <you@example.com>
2021-05-27 18:26:05 -0500

    Started Exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -16,17 +17,25 @@ import java.util.List;
 public class EventController {
     private static List<String> events = new ArrayList<>();
 
-    @GetMapping
-    public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        model.addAttribute("events",events);
-        return "events/index";
-    }
+    HashMap<String, String> events1 = new HashMap<String, String>();
+    events1.put("Strangeloop", "A Big coding evet in STL at the Stifel.");
+    events1.put("Battle of the Bagels", "A showdown for the yummiest bread in town.");
+    events1.put("Meshuggah", "BYO Pretzels");
+
+    return events1;
+//    @GetMapping
+//    public String displayAllEvents(Model model){
+//        List<String> events = new ArrayList<>();
+//        model.addAttribute("events",events);
+//        return "events/index";
+
+
     //lives at /events/create
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
     }
+
     //lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName){
